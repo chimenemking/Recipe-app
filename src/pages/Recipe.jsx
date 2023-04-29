16c9cc4eb0138285db4f9c5e32bb17b52f389aac
chimenem Okandu <chimenemwillis@gmail.com>
2023-04-29 02:34:33 +0100

    all changes

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -3,21 +3,21 @@ import { useParams } from 'react-router-dom'
 
 function Recipe() {
 
-    let params=useParams();
-    const{details,setDetails}= useState({});
+    let params= useParams();
+    const [details, setDetails]= useState({});
 
-const fecthDetails = async ()=>{
+const fetchDetails = async ()=>{
     const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
    const detailData = await data.json();//to save the data that is fetched from the api in (const data)
  setDetails(detailData)  //set the data that was saved in [const detailData] which was originally fetched from [const data] and what is being fetched is an object and that's the reason for the{} in useState({})
 }
 
 useEffect(()=>{
-    fecthDetails();
+    fetchDetails();
 },[params.name])
   return (
     <div>
-        <h1>recipe is something </h1>
+         {details.title}
     </div>
   )
 }
